a67d606f5494ac140f1d4ad548f8a79223065f17
saalik123 <63722568+saalik123@users.noreply.github.com>
2020-06-30 23:12:02 +0530

    settings.py updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -135,8 +135,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'salikmubien@gmail.com'
-EMAIL_HOST_PASSWORD = 'eujjjnbrjtptdggs'
-
-# DEFAULT_FROM_EMAIL = 'salikmubien@gmail.com'
-# SERVER_EMAIL = 'salikmubien@gmail.com'
+EMAIL_HOST_USER = 'youremail@gmail.com'
+EMAIL_HOST_PASSWORD = '***************'
